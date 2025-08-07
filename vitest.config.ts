faf8d8d5edf2cf76e76f5df200332cbbd05698aa
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2025-08-07 14:47:58 +0900

    add vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,28 @@
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    globals: false,
+    environment: 'node',
+    coverage: {
+      provider: 'v8',
+    },
+    env: {
+      APP_DATABASE_URL: process.env['APP_TEST_DATABASE_URL'],
+    },
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: 'A',
+          include: ['src/infrastructure/postgres/__test__/A_each-commit-trancate/**/*.ts'],
+          setupFiles:
+            './src/infrastructure/postgres/__test__/A_each-commit-trancate/+each-file-setup.ts',
+          globalSetup: './src/infrastructure/postgres/__test__/+global-setup.ts',
+        },
+      },
+    ],
+  },
+});
